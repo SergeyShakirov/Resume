d08f9a5c3907f47b0aa56820795f1c9bb6ef238b
Sergey <59853686+SergeyShakirov@users.noreply.github.com>
2020-02-23 11:24:56 +0600

    Заполнение сайта

--- assets/data.php
+++ assets/data.php
@@ -1,7 +1,32 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Boss
- * Date: 01.04.2019
- * Time: 8:53
- */
+return [
+  'education'=> [
+    'faculty' => 'Mining faculty',
+    'univercity' => 'Karaganda State Technical University',
+    'yearStart' => 2009,
+    'yearEnd' => 2013
+  ],
+
+  'about' => [
+    'name' => 'Sergey Shakirov',
+    'post' => 'Developer',
+    'email' => 'Shakirsergey@gmail.com',
+    'phone' => '+7 705 588 8668',
+    'git' => 'SergeyShakirov'
+  ],
+
+  'shortBiography' => 'Данный сайт разработан собственноручно с применением кода PHP и Javascript.
+                      С дальнейшим прокачиванием навыков программирования, сайт будет преображаться.
+                      Как создавался исходный код можно рассмотреть на GitHub',
+
+  'experience' => [
+    'post' => 'Разработчик 1с',
+    'workPlace' => '1c Франчайзи, Караганда',
+    'yearStart' => 2019,
+    'yearEnd' => 'Настоящее время',
+    'description' => 'Разработка, доработка,
+                      исправление работы конфигурации 1с, правил обмена,
+                      правил регистрации, интеграции с различными системами'
+  ]
+
+];
